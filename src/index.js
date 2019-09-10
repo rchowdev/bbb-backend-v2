4dd895f64161f730bb5f46e028392e8705b4a55e
rchow1944 <rchow2@buffalo.edu>
2019-09-09 23:47:46 -0400

    Tested querying data from GraphQL

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import Query from './resolvers/Query'
 import Mutation from './resolvers/Mutation'
 import User from './resolvers/User'
 import Book from './resolvers/Book'
+import './prisma'
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
--- src/prisma.js
+++ src/prisma.js
@@ -2,5 +2,8 @@ import { Prisma } from 'prisma-binding';
 
 const prisma = new Prisma({ 
     typeDefs: 'src/generated/prisma.graphql',
-    endpoint: 'localhost:4466'
-})
+    endpoint: 'http://localhost:4466'
+});
+
+prisma.query.users(null, '{ name email books { id title } }')
+    .then(data => console.log(JSON.stringify(data, undefined, 4)));

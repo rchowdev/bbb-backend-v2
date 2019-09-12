4544b8c2f1af3fd427a5fb4cfe1f3681b6120f7a
rchow1944 <rchow2@buffalo.edu>
2019-09-12 02:19:17 -0400

    Defined create book function

--- src/prisma.js
+++ src/prisma.js
@@ -8,16 +8,41 @@ const prisma = new Prisma({
 // prisma.query.users(null, '{ name email books { id title } }')
 //     .then(data => console.log(JSON.stringify(data, undefined, 4)));
 
-prisma.mutation.createBook({
-    data: {
-        title: "Test Book",
-        author: "Test Author",
-        users: {
-            connect: {
-                id: "cjzxsgbdj00f3077198sss8s4"
+// prisma.mutation.createBook({
+//     data: {
+//         title: "Test Book",
+//         author: "Test Author",
+//         users: {
+//             connect: {
+//                 id: "cjzxsgbdj00f3077198sss8s4"
+//             }
+//         }
+//     }
+// }, '{ id title users { email } }').then(data => {
+//     console.log(JSON.stringify(data, undefined, 4))
+// })
+
+const createBookForUser = async (userId, data) => {
+    const book = await prisma.mutation.upsertBook({
+        where: {
+            isbn: data.isbn
+        },
+        create: {
+            ...data,
+            users: {
+                connect: {
+                    id: userId
+                }
+            }
+        },
+        update: {
+            users: {
+                connect: {
+                    id: userId
+                }
             }
         }
-    }
-}, '{ id title users { email } }').then(data => {
-    console.log(JSON.stringify(data, undefined, 4))
-})
+    }, '{ id title users { name } }')
+
+    return book
+}

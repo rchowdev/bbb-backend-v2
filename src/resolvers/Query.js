b669daf0529e259ac25b80da73da5115e5067500
rchow1944 <rchow2@buffalo.edu>
2019-09-14 17:00:55 -0400

    users query return users information from pg database

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -3,9 +3,10 @@ const Query = {
         return db.users.find(user => user.id === args.id)
     },
     users(parent, args, { db, prisma }, info) {
-        return args.query 
-            ? db.users.filter(user => user.name.toLowerCase().includes(args.query.toLowerCase()))
-            : db.users
+        // return args.query 
+        //     ? db.users.filter(user => user.name.toLowerCase().includes(args.query.toLowerCase()))
+        //     : db.users
+        return prisma.query.users(null, info)
     },
     book(parent, args, { db }, info) {
         return db.books.find(book => book.id === args.id)

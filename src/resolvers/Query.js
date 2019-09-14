f57203d25010f6d147a2a69133d4a9290f6b9dff
rchow1944 <rchow2@buffalo.edu>
2019-09-14 17:03:49 -0400

    Removed db from context. Added prisma to books query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,7 @@ const Query = {
     user(parent, args, { db }, info) {
         return db.users.find(user => user.id === args.id)
     },
-    users(parent, args, { db, prisma }, info) {
+    users(parent, args, { prisma }, info) {
         // return args.query 
         //     ? db.users.filter(user => user.name.toLowerCase().includes(args.query.toLowerCase()))
         //     : db.users
@@ -11,8 +11,8 @@ const Query = {
     book(parent, args, { db }, info) {
         return db.books.find(book => book.id === args.id)
     },
-    books(parent, args, { db }, info) {
-        return db.books
+    books(parent, args, { prisma }, info) {
+        return prisma.query.books(null, info)
     }
 }
 

c419732e3866c24e23e1e5c1af09a67c59bd4fc6
rchow1944 <rchow2@buffalo.edu>
2019-09-14 16:53:42 -0400

    Added prisma to users query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -2,7 +2,7 @@ const Query = {
     user(parent, args, { db }, info) {
         return db.users.find(user => user.id === args.id)
     },
-    users(parent, args, { db }, info) {
+    users(parent, args, { db, prisma }, info) {
         return args.query 
             ? db.users.filter(user => user.name.toLowerCase().includes(args.query.toLowerCase()))
             : db.users

a74f972061ccdc4d11d3aa538653ef541c9808d1
rchow1944 <rchow2@buffalo.edu>
2019-09-14 16:46:56 -0400

    Added export statement

--- src/prisma.js
+++ src/prisma.js
@@ -5,6 +5,8 @@ const prisma = new Prisma({
     endpoint: 'http://localhost:4466'
 });
 
+export default prisma;
+
 // prisma.query.users(null, '{ name email books { id title } }')
 //     .then(data => console.log(JSON.stringify(data, undefined, 4)));
 
